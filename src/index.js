f4084b4bc523911a1ee0e6aa3015ce8a54f9ff98
Quendara <andregarstka@web.de>
2020-01-09 17:20:59 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,23 +4,19 @@ import ReactDOM from "react-dom";
 import Board from "./Board";
 
 import { messageService } from "./messageService";
-
 import { HomePage } from "./HomePage";
 import { MessageReciever } from "./MessageReciever";
 
 import { jsonToCSV, csvSoJson } from "./csvToJson";
 
-
 // import { groupBy,uniq,map } from 'underscore'
-
-
 
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { List, DatePicker, message } from "antd";
 
 const getRandElement = arr => {
-  const number = Math.floor(Math.random() * Math.floor(arr.length))
-  console.log(number)
+  const number = Math.floor(Math.random() * Math.floor(arr.length));
+  console.log(number);
   return arr[number];
 };
 
@@ -29,7 +25,7 @@ const storyAttributes = {
   epic: ["epic1", "epic2", "epic3", "epic4"],
   prio: ["0", "2", "5", "10"],
   start: ["Q1", "Q2", "Q3", "Q4"]
-}
+};
 
 // fake data generator
 const getItems = (count, offset = 0) =>
@@ -42,104 +38,69 @@ const getItems = (count, offset = 0) =>
     version: "v" + getRandElement(["1.0", "1.1", "2.0"]),
     prio: "" + getRandElement(storyAttributes.prio),
     status: "" + getRandElement(storyAttributes.status),
-    start: "" + getRandElement(storyAttributes.start),
+    start: "" + getRandElement(storyAttributes.start)
   }));
 
+function testCSV(jsonInput) {
+  console.log("Print flatlist");
+  console.log(jsonInput);
 
-function testCSV( jsonInput )
-{
-    console.log("Print flatlist")
-    console.log(jsonInput)
+  const csv = jsonToCSV(jsonInput);
 
-    const csv = jsonToCSV( jsonInput )
+  console.log("Print csv");
+  console.log(csv);
 
-    console.log("Print csv")
-    console.log(csv)
+  const json2 = csvSoJson(csv);
 
-    const json2 = csvSoJson( csv )
+  console.log("Print json");
+  console.log(json2);
 
-    console.log("Print json")
-    console.log(json2)
+  const csv2 = jsonToCSV(json2);
 
-    const csv2 = jsonToCSV( json2 )
-    
-    console.log("Print csv")
-    console.log(csv2)    
+  console.log("Print csv");
+  console.log(csv2);
 }
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class FileOpen extends Component {
-
   constructor(props) {
     super(props);
   }
 
-  showFile = async (e) => {
-    e.preventDefault()
-    const reader = new FileReader()
-    reader.onload = async (e) => { 
-      const text = (e.target.result)
-      console.log(text)
-      alert(text)
-      messageService.sendMessage(text)
+  showFile = async e => {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.onload = async e => {
+      const text = e.target.result;
+      console.log(text);
+      alert(text);
+      messageService.sendMessage(text);
     };
-    reader.readAsText(e.target.files[0])
-  }
+    reader.readAsText(e.target.files[0]);
+  };
 
   render = () => {
-
-    return (<div>
-      <input type="file" onChange={(e) => this.showFile(e)} />
-    </div>
-    )
-  }
+    return (
+      <div>
+        <input type="file" onChange={e => this.showFile(e)} />
+      </div>
+    );
+  };
 }
 
 export default App;
-
 
 class App extends Component {
   constructor() {
     super();
 
-    
-    const flatlist = getItems(10)
+    const flatlist = getItems(10);
+    // testCSV(flatlist);
 
-    testCSV( flatlist  )
-   
-    this.lists = []
+    this.state.lists = [];
 
-    const groupBy = "start"
-    // const groupedList = groupBy( flatlist, groupBy ) 
-
-    const groups = storyAttributes[groupBy]
-
-    // create groups / colums
-    groups.map((item, index) => {
-      this.lists.push({ title: item, items: [] })
-    })
-
-    // add items to the columns
-    flatlist.map((listitem, index) => {
-
-      const groupItem = listitem[groupBy]
-      const colIdx = groups.indexOf(groupItem)
-
-      // push items to the correct column
-      if (colIdx >= 0) {
-        this.lists[colIdx].items.push(listitem)
-      }
-      else {
-        console.warn("Item Ignored")
-        console.warn(listitem)
-      }
-
-
-
-      // if( listitem[ groupBy ] === 'Done' )
-
-    })
+    this.setJsonData(flatlist);
 
     // this.lists = [
     //   {
@@ -150,23 +111,65 @@ class App extends Component {
     // ]; // lists
   }
 
-  componentDidMount() { }
+  setCSVData(csv) {
+
+    const flatlist = csvSoJson( csv )
+    this.setJsonData(flatlist)
+  }
+
+
+   setJsonData(flatlist) {
+
+    // const groupBy = "start";
+    // // const groupedList = groupBy( flatlist, groupBy )
+
+    // const groups = storyAttributes[groupBy];
+
+    // // create groups / colums
+    // groups.map((item, index) => {
+    //   this.state.lists.push({ title: item, items: [] });
+    // });
+ 
+    // // add items to the columns
+    // flatlist.map((listitem, index) => {
+    //   const groupItem = listitem[groupBy];
+    //   const colIdx = groups.indexOf(groupItem);
+
+    //   // push items to the correct column
+    //   if (colIdx >= 0) {
+    //     this.state.lists[colIdx].items.push(listitem);
+    //   } else {
+    //     console.warn("Item Ignored");
+    //     console.warn(listitem);
+    //   }
+    // });
+  }
+
+  componentDidMount() {
+    // subscribe to home component messages
+    this.subscription = messageService.getMessage().subscribe(message => {
+      if (message) {
+        // add message to local state if not empty
+        this.setCSVData( message )
+        // this.setState({ messages: [...this.state.messages, message] });
+      } else {
+        // clear messages when empty message received
+        // this.setState({ messages: [] });
+      }
+    });
+  }
 
   render() {
     // <HomePage />
     return (
       <div>
         <div className="container text-center">
-          
           <MessageReciever />
           <FileOpen />
           <div className="row">
-            <Board list={this.lists} />
-
+            <Board list={this.state.lists} />
           </div>
         </div>
-
-
       </div>
     );
   }
