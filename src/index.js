e720c068b1ab18f6f6419304fde2003395c472c7
Quendara <andregarstka@web.de>
2020-01-09 15:44:44 +0100

    1 file modified, 1 added

--- src/index.js
+++ src/index.js
@@ -67,6 +67,37 @@ function testCSV( jsonInput )
     console.log(csv2)    
 }
 
+import React, { Component } from 'react';
+
+class FileOpen extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = async (e) => { 
+      const text = (e.target.result)
+      console.log(text)
+      alert(text)
+      messageService.sendMessage(text)
+    };
+    reader.readAsText(e.target.files[0])
+  }
+
+  render = () => {
+
+    return (<div>
+      <input type="file" onChange={(e) => this.showFile(e)} />
+    </div>
+    )
+  }
+}
+
+export default App;
+
 
 class App extends Component {
   constructor() {
@@ -122,11 +153,13 @@ class App extends Component {
   componentDidMount() { }
 
   render() {
+    // <HomePage />
     return (
       <div>
         <div className="container text-center">
-          <HomePage />
+          
           <MessageReciever />
+          <FileOpen />
           <div className="row">
             <Board list={this.lists} />
 
